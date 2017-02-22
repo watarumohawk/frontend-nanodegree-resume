2ff2eecd8dc3ba5ce0f0fda4706a918a4acd1f44
Wataru Sekiguchi <wataru.sekiguchi416@gmail.com>
2017-02-22 16:48:30 +0900

    added data

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,47 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var bio = {
+    'name': 'Wataru Sekiguchi',
+    'role': 'Front-end Developer',
+    'contacts': {
+        'mobile': '080111111111',
+        'email': 'wataru.sekiguchi416@gmail.com',
+        'github': 'watarumohawk',
+        'twitter': '@watarumohawk',
+    },
+    'location': 'Tokyo',
+    'welcomeMessage': "Hi, I'm Wataru",
+    'skills': ['JavaScript'],
+    'biopic': 'images/fry.jpg'
+};
+
+var education = {
+    'schools': {
+        'name': 'Nihon University',
+        'location': 'Tokyo',
+        'degree': 'Bachelor of Science',
+        'majors': ['Physics'],
+        'dates': '2012 - 2016'
+    },
+    'onlineCourses': {
+        'title': 'Front-end Web Developer',
+        'school': 'Udacity',
+        'dates': 'December, 2016 - Present'
+    }
+};
+
+
+var work = {
+    'jobs': [],
+    'employer': 'Hays',
+    'title': 'Technical Solutions Specialist',
+    'location': 'Tokyo',
+    'dates': 'in progress',
+    'description': 'string'
+};
+
+var projects = {
+    'projects': [],
+    'title': '',
+    'dates': '',
+    'description': '',
+    'images': []
+};
